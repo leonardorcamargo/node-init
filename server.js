a2305b1deaa23232294273ad6470393ad345f572
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-07-23 16:56:34 -0300

    Inserindo Middlewares para serem utilizados quando endpoints forem chamados no servidor

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ import express from 'express';
 import bodyParser from 'body-parser';
  
 const app = express();
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use((req, res, next) => {
+	console.log('Will run before any route');
+	next();
+});
+
+app.use('/users',(req, res, next) => {
+	console.log('Will run before users route');
+	next();
+});
+
+app.get('/', (req, res) => {
+	console.log('Route / called')
+	res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+	console.log('Route /users called');
+	res.send('Hey! You called the users!');
+})
 
 app.listen(3000, () => {
 	console.log('Example app listening on port 3000!');
