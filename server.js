8275148a41de59d5e98d9ca29494d25c4737ca5f
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-07-23 23:31:06 -0300

    Adicionado arquivo /src/app.js para carregar os middlewares e iniciar o express. Arquivo server.js ficou com responsabilidade apenas de inicar a aplicação.

--- server.js
+++ server.js
@@ -1,35 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
- 
-const app = express();
+import app from './src/app';
+const port = 3000;
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-	console.log('Will run before any route');
-	next(new Error('failed!'));
-});
-
-app.use('/users',(req, res, next) => {
-	console.log('Will run before users route');
-	next();
-});
-
-app.get('/', (req, res) => {
-	console.log('Route / called')
-	res.send('Hello World!');
-});
-
-app.use((err, req, res, next) => {
-	console.log('Something goes wrong!');
-	res.status(500).send(err.message);
-});
-
-app.get('/users', (req, res) => {
-	console.log('Route /users called');
-	res.send('Hey! You called the users!');
-})
-
-app.listen(3000, () => {
-	console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`);
 });
---
+++ src/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+ 
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/error', (req, res, next) => {
+	console.log('Will run before error route');
+	next(new Error('failed!'));
+});
+
+app.use('/users',(req, res, next) => {
+	console.log('Will run before users route');
+	next();
+});
+
+app.use((err, req, res, next) => {
+	console.log('Something goes wrong!');
+	res.status(500).send(err.message);
+});
+
+app.get('/', (req, res) => {
+	console.log('Route / called')
+	res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+	console.log('Route /users called');
+	res.send('Hey! You called the users!');
+});
+
+app.get('/error', (req, res) => {
+	console.log('Route /error called');
+	res.send('Hey! You called the error!');
+});
+
+export default app;
